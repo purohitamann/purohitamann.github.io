e4c5bd92f0918bb116788e4c5cf06fe3ecc630cc
Aman Purohit <purohiam@sheridancollege.ca>
2024-03-21 01:00:15 -0400

    navbar setup complete

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon} from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src="" alt="" className="logo" />
+      
+      <h1 className="logo" >@puroihitamann</h1>
       <ul>
         <li>Home</li>
         <li>Projects</li>
@@ -12,10 +17,11 @@ const Navbar = () => {
 
       <div className="search-box">
         <input type="text" placeholder="Search" />
-        <img src="" alt="" />
+        <FontAwesomeIcon className="fontAwesome" icon={faMagnifyingGlass} />
       </div>
 
-      <img src="" alt="" className="toggle-icon" />
+
+      <FontAwesomeIcon className="toggle-icon" icon={faMoon} />
     </div>
   );
 };
